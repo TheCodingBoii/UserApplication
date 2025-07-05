2e216c85ee00f1db02193efece236feade2b456b
Shan Gunarathne <madusoftnet91@gmail.com>
2025-07-05 19:01:09 +0530

    Add project files.

---
+++ UserApplication/Data/AppDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using UserApplication.Models;
+
+namespace UserApplication.Data
+{
+    public class AppDBContext : IdentityDbContext<Users>
+    {
+        public AppDBContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        
+    }
+}
---
+++ UserApplication/Models/Users.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace UserApplication.Models
+{
+    public class Users : IdentityUser
+    {
+
+        public String FullName { get; set; }
+    }
+}
